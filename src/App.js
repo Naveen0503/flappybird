ec4fa8808cd37c60f237517d65fa5e726eed0113
Kumar <avu-naveen.kumar@capgemini.com>
2023-04-25 19:30:38 +0530

    flappybird

--- src/App.js
+++ src/App.js
@@ -1,25 +1,162 @@
-import logo from './logo.svg';
+
 import './App.css';
+import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+
+const BIRD_SIZE = 40;
+const GAME_WIDTH = 1000;
+const GAME_HEIGHT = 500;
+const GRAVITY = 6;
+const JUMP_HEIGHT = 100;
+const OBSTACLE_WIDTH = 52;
+const OBSTACLE_GAP = 200;
 
 function App() {
+
+const [birdposition,setbirdposition] = useState(250);
+const [gamehasstarted,setgamehasstarted] = useState(false);
+const [obstacleheight,setobstacleheight] = useState(50);
+const [obstacleleft,setobstacleleft] = useState(GAME_WIDTH-OBSTACLE_WIDTH)
+const [score,setscore] = useState(0);
+const [visibility,setvisibility]= useState(true);
+
+const bottomobstacleheight = GAME_HEIGHT - OBSTACLE_GAP - obstacleheight;
+useEffect(() => {
+  let timeid ;
+  if(gamehasstarted&&birdposition < GAME_HEIGHT - BIRD_SIZE){
+    timeid = setInterval(() => {
+      setbirdposition((birdposition)=> birdposition + GRAVITY)
+    },24);
+  }
+  return () => {
+   clearInterval(timeid);
+  };
+},[birdposition,gamehasstarted]);
+ 
+useEffect(() => {
+let obstacleid;
+if(gamehasstarted && obstacleleft >= -OBSTACLE_WIDTH)
+{
+  obstacleid = setInterval(() => {
+    setobstacleleft((obstacleleft) => obstacleleft - 5);
+  }, 24);
+  return () => {
+    clearInterval(obstacleid)
+  }
+}
+else
+{
+  setobstacleleft(GAME_WIDTH-OBSTACLE_WIDTH);
+  setobstacleheight(Math.floor(Math.random() * (GAME_HEIGHT-OBSTACLE_GAP)));
+  setscore(score => score + 1);
+}
+},[gamehasstarted,obstacleleft]);
+
+useEffect(() => {
+  const hascollidedwithtopobstacle = birdposition >= 0 && birdposition < obstacleheight ;
+  const hascollidedwithbottomobstacle = birdposition <= GAME_HEIGHT && birdposition >= GAME_HEIGHT - bottomobstacleheight ;
+
+  if( birdposition <= 0 || (obstacleleft >= 0 && obstacleleft <= OBSTACLE_WIDTH && 
+    (hascollidedwithtopobstacle || hascollidedwithbottomobstacle)))
+    {
+      setgamehasstarted(false)
+      setvisibility(true)
+      setscore(0);
+    }
+
+},[birdposition,obstacleheight,bottomobstacleheight,obstacleleft])
+
+const handleclick = () =>{
+  let newbirdposition = birdposition - JUMP_HEIGHT;
+  if(!gamehasstarted){
+    setgamehasstarted(true);
+    setvisibility(false);
+  }
+  else if(newbirdposition < 0){
+    setbirdposition(0)
+  }
+  else{
+  setbirdposition(newbirdposition);
+  }
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div onClick={handleclick}>
+      <GameBox height={GAME_HEIGHT} width={GAME_WIDTH}>
+        {visibility ? <Startboard>tap to start</Startboard> : null}
+      <Obstacle
+        top = {0}
+        width ={OBSTACLE_WIDTH}
+        height ={obstacleheight} 
+        left = {obstacleleft}
+        deg ={180}/>
+
+      <Obstacle
+        top = {GAME_HEIGHT - (obstacleheight + bottomobstacleheight)}
+        width ={OBSTACLE_WIDTH}
+        height ={bottomobstacleheight} 
+        left = {obstacleleft}
+        deg ={0}/>
+      <Bird size={BIRD_SIZE} top = {birdposition}/>
+      </GameBox>
+      <span>{score}</span>
+    </Div>
   );
 }
 
 export default App;
+
+
+const Bird = styled.div`
+ position: absolute;
+ background-image: url("./images/bird.png");
+  background-repeat: no-repeat;
+  background-size: ${(props) => props.width}px ${(props) => props.height}px;
+ height: ${(props) => props.size}px;
+ width: ${(props) => props.size}px;
+ top: ${(props) => props.top}px;
+ border-radius: 50%;
+ `;
+
+const Div = styled.div`
+display: flex;
+width: 100%;
+justify-content: center;
+& span{
+  color: white;
+  font-size: 24px;
+  position: absolute;
+}
+`;
+
+const GameBox = styled.div`
+height: ${(props) =>props.height}px;
+width: ${(props) =>props.width}px;
+
+background-image: url("./images/img.png");
+overflow: hidden;
+`;
+
+const Obstacle = styled.div`
+position: relative;
+background-image: url("./images/pipe-green.png");
+width: ${(props) => props.width}px;
+height: ${(props) => props.height}px;
+left: ${(props) => props.left}px;
+top: ${(props) => props.top}px;
+transform: rotate(${(props) => props.deg}deg);
+`;
+
+const Startboard = styled.div`
+  position: absolute;
+  top: 49%;
+  background-color: black;
+  padding: 10px;
+  width: 100px;
+  left: 50%;
+  margin-left: -50px;
+  text-align: center;
+  font-size: 20px;
+  border-radius: 10px;
+  color: #fff;
+  font-weight: 600;
+`;
